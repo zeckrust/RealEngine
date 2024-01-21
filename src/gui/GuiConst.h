ce6465e2cdec9f451dd8891775c76fb8972562c0
Gabriel Bergeron <gabriel.bergeron.protic@gmail.com>
2024-01-21 18:05:31 -0500

    changed padding constant

--- src/gui/GuiConst.h
+++ src/gui/GuiConst.h
@@ -8,7 +8,7 @@ const int BACKGROUND_COLOR_B = 120;
 // Title bar
 const float TITLE_BAR_POS_X = 0.0f;
 const float TITLE_BAR_POS_Y = 0.0f;
-const float TITLE_BAR_BUTTON_PADDING = 5.0f;
+const float TITLE_BAR_BUTTON_PADDING = 10.0f;
 
 const int TITLE_BAR_HEIGHT = 30;
 const int TITLE_BAR_COLOR_R = 66;
@@ -24,4 +24,4 @@ const int BUTTON_FONT_SIZE = 11;
 // Fonts
 const std::string REGULAR_FONT = "../src/gui/fonts/OpenSans-Regular.ttf";
 const std::string BOLD_FONT = "../src/gui/fonts/OpenSans-Bold.ttf";
-const int FONT_WIDTH_ERROR = 10;
+const float FONT_WIDTH_ERROR = 8.0f;
