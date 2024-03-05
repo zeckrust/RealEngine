9833c91d7f65def3ea53fff7672246f1ff2a3f03
Gabriel Bergeron <gabriel.bergeron.protic@gmail.com>
2024-03-05 12:44:43 -0500

    Added color mode HSB

--- src/gui/panels/ColorPanel.cpp
+++ src/gui/panels/ColorPanel.cpp
@@ -1,24 +1,35 @@
 #include "ColorPanel.h"
+#include <cmath>
 
 ColorPanel::ColorPanel() : CustomPanel() {
-	slider1.addListener(this, &ColorPanel::handleSliderChanged);
-	slider2.addListener(this, &ColorPanel::handleSliderChanged);
-	slider3.addListener(this, &ColorPanel::handleSliderChanged);
+	enableListeners();
 }
 
 void ColorPanel::setup(std::string panelName, float x, float y, ofColor defaultColor) {
 	CustomPanel::setup(panelName, x, y);
+	setupButton();
 	setupSliders(defaultColor);
-	currentColorMode = RGB;
 	currentColor = defaultColor;
 	minimize();
 }
 
+void ColorPanel::setupButton(void) {
+	colorModeButton.setup(RGB_STR, REGULAR_FONT, 0, 0, DEFAULT_BUTTON_HEIGHT);
+	add(&colorModeButton);
+}
+
+void ColorPanel::setupSliders(ofColor defaultColor) {
+	slider1.setup(R_STR, defaultColor.r, 0, 255);
+	slider2.setup(G_STR, defaultColor.g, 0, 255);
+	slider3.setup(B_STR, defaultColor.b, 0, 255);
+
+	add(&slider1);
+	add(&slider2);
+	add(&slider3);
+}
+
 void ColorPanel::update(void) {
 	CustomPanel::update();
-
-	glm::vec3 pos = getPosition();
-	colorSampleRect = ofRectangle(pos.x, pos.y, getWidth(), DEFAULT_BUTTON_HEIGHT);
 
 	if (!ofGetMousePressed()) {
 		showColorSample = false;
@@ -42,36 +53,37 @@ void ColorPanel::draw(void) {
 	}
 }
 
-void ColorPanel::setupSliders(ofColor defaultColor) {
-	slider1.setup("R", defaultColor.r, 0, 255);
-	slider2.setup("G", defaultColor.g, 0, 255);
-	slider3.setup("B", defaultColor.b, 0, 255);
-
-	add(&slider1);
-	add(&slider2);
-	add(&slider3);
+bool ColorPanel::mousePressed(ofMouseEventArgs& args) {
+	if (colorModeButton.getShape().inside(args.x, args.y)) {
+		changeColorMode();
+	}
+	return CustomPanel::mousePressed(args);
 }
 
-void ColorPanel::setColorMode(ColorMode mode) {
-	currentColorMode = mode;
+void ColorPanel::changeColorMode(void) {
+	disableListeners();
+	if (currentColorMode == RGB ? currentColorMode = HSB : currentColorMode = RGB);
 
-	switch (mode) {
+	switch (currentColorMode) {
 		case RGB: {
-			slider1.setup("R", currentColor.r, 0, 255);
-			slider2.setup("G", currentColor.g, 0, 255);
-			slider3.setup("B", currentColor.b, 0, 255);
+			slider1.setup(R_STR, currentColor.r, 0, 255);
+			slider2.setup(G_STR, currentColor.g, 0, 255);
+			slider3.setup(B_STR, currentColor.b, 0, 255);
+			colorModeButton.setup(RGB_STR, REGULAR_FONT, 0, 0, DEFAULT_BUTTON_HEIGHT);
 			break;
 		}
 		case HSB: {
-			slider1.setup("H", currentColor.getHue() * 360, 0, 360);
-			slider2.setup("S", currentColor.getSaturation() * 100, 0, 100);
-			slider3.setup("B", currentColor.getBrightness() * 100, 0, 100);
+			slider1.setup(H_STR, currentColor.getHueAngle(), 0, 360);
+			slider2.setup(S_STR, TO_SLIDER_VALUE(currentColor.getSaturation()), 0, 100);
+			slider3.setup(B_STR, TO_SLIDER_VALUE(currentColor.getBrightness()), 0, 100);
+			colorModeButton.setup(HSB_STR, REGULAR_FONT, 0, 0, DEFAULT_BUTTON_HEIGHT);
 			break;
 		}
 		default: {
 			break;
 		}
 	}
+	enableListeners();
 }
 
 ofColor ColorPanel::getColor(void) {
@@ -81,13 +93,18 @@ ofColor ColorPanel::getColor(void) {
 void ColorPanel::handleSliderChanged(int &_) {
 	showColorSample = true;
 
+	glm::vec3 pos = colorModeButton.getPosition();
+	colorSampleRect = ofRectangle(pos.x, pos.y, getWidth(), DEFAULT_BUTTON_HEIGHT);
+
 	switch (currentColorMode) {
 		case RGB: {
 			currentColor.set(slider1, slider2, slider3);
 			break;
 		}
 		case HSB: {
-			currentColor.setHsb(slider1 / 360, slider2 / 100, slider3 / 100);
+			currentColor.setHueAngle(slider1);
+			currentColor.setSaturation(TO_HSB_VALUE(slider2));
+			currentColor.setBrightness(TO_HSB_VALUE(slider3));
 			break;
 		}
 		default: {
@@ -96,4 +113,16 @@ void ColorPanel::handleSliderChanged(int &_) {
 	}
 
 	setBorderColor(currentColor);
+}
+
+void ColorPanel::enableListeners() {
+	slider1.addListener(this, &ColorPanel::handleSliderChanged);
+	slider2.addListener(this, &ColorPanel::handleSliderChanged);
+	slider3.addListener(this, &ColorPanel::handleSliderChanged);
+}
+
+void ColorPanel::disableListeners() {
+	slider1.removeListener(this, &ColorPanel::handleSliderChanged);
+	slider2.removeListener(this, &ColorPanel::handleSliderChanged);
+	slider3.removeListener(this, &ColorPanel::handleSliderChanged);
 }
--- src/gui/panels/ColorPanel.h
+++ src/gui/panels/ColorPanel.h
@@ -1,6 +1,10 @@
 #pragma once
 
 #include "CustomPanel.h"
+#include "../buttons/CustomButton.h"
+
+#define TO_SLIDER_VALUE(valueHSB) (int)ceil(valueHSB / 256. * 100.)
+#define TO_HSB_VALUE(valueSlider) (float)valueSlider * 256. / 100.
 
 enum ColorMode {
 	RGB = 0,
@@ -14,23 +18,38 @@ class ColorPanel : public CustomPanel
 		void setup(std::string panelName, float x, float y, ofColor defaultColor);
 		void update(void);
 		void draw(void);
+		bool mousePressed(ofMouseEventArgs& args);
 
-		void setColorMode(ColorMode mode);
+		void changeColorMode(void);
 		ofColor getColor(void);
 
 	private:
 		void setupSliders(ofColor defaultColor);
+		void setupButton(void);
 		void handleSliderChanged(int &value);
+
+		void enableListeners(void);
+		void disableListeners(void);
 
 		ofxIntSlider slider1;
 		ofxIntSlider slider2;
 		ofxIntSlider slider3;
 
-		ColorMode currentColorMode;
+		CustomButton colorModeButton;
+		ColorMode currentColorMode = RGB;
 		ofColor currentColor;
 
 		ofRectangle colorSampleRect;
 		bool showColorSample = false;
 
 		const int BORDER_LINE_WIDTH = 2;
+		const std::string RGB_STR = "\tRGB";
+		const std::string HSB_STR = "\tHSB";
+
+		const std::string R_STR = "\tR";
+		const std::string G_STR = "\tG";
+		const std::string B_STR = "\tB";
+
+		const std::string H_STR = "\tH";
+		const std::string S_STR = "\tS";
 };
