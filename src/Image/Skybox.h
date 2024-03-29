d5c7e65b4015fa20e81bcfd64d5636acd0126b31
Math-Fauch <faucher.mathieu@gmail.com>
2024-03-29 16:47:07 -0400

    optimisation for application speed

--- src/Image/Skybox.h
+++ src/Image/Skybox.h
@@ -4,14 +4,16 @@
 
 class Skybox : public SceneObject{
 public:
-	void draw();
+	void draw() {};
+	void setup();
 	void set_texture(ofImage _image);
 	static ofImage generate_texture();
+	void draw_mesh();
+
 private: 
 	void calculate_vertex();
 	void calculate_normal();
 	void calculate_tex_coord(int w, int h);
-	void draw_mesh();
 
 	ofMesh mesh;
 	ofImage image;
