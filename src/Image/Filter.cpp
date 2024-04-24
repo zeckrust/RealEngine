91c278fe7f95fdb1f2e94549db3108d3ac1b5e87
Gabriel Bergeron <gabriel.bergeron.protic@gmail.com>
2024-04-23 21:08:59 -0400

    adjusted filters

--- src/Image/Filter.cpp
+++ src/Image/Filter.cpp
@@ -29,19 +29,11 @@ ofImage Filter::toKelvin(ofImage& texture) {
 
     for (int y = 0; y < height; y++) {
         for (int x = 0; x < width; x++) {
-            ofColor color = filteredImage.getColor(x, y);
-
-            float red = color.r / 255.0;
-            float green = color.g / 255.0;
-            float blue = color.b / 255.0;
-
-            float temp = 0.5;
-
-            float redKelvin = red * (temp <= 66 ? 1 : (float)(temp - 60) / (temp <= 66 ? 66 : 255));
-            float greenKelvin = green * (temp <= 66 ? (float)((temp <= 66 ? 99.4708025861 : temp - 60.0) * log(temp <= 66 ? temp : 255 - temp)) - 161.1195681661 : (float)(temp - 60) / (temp <= 66 ? 66 : 255));
-            float blueKelvin = (temp >= 66 ? 1 : temp <= 19 ? 0 : (float)((temp <= 66 ? 138.5177312231 : temp - 10) * log(temp <= 66 ? temp : 255 - temp)) - 305.0447927307);
-
-            filteredImage.setColor(x, y, ofColor(redKelvin * 255, greenKelvin * 255, blueKelvin * 255));
+            ofColor pixel_color = filteredImage.getColor(x, y);
+            ofColor kelvin = ofColor(255, 178, 144);
+            ofColor filtered_color = pixel_color.lerp(kelvin, 0.5);
+            
+            filteredImage.setColor(x, y, filtered_color);
         }
     }
 
@@ -64,14 +56,14 @@ ofImage Filter::toNashville(ofImage& texture) {
             float g = color.g;
             float b = color.b;
 
-            r = 0.393 * r + 0.769 * g + 0.189 * b;
-            g = 0.349 * r + 0.686 * g + 0.168 * b;
-            b = 0.272 * r + 0.534 * g + 0.131 * b;
+            r = ofClamp(0.9 * r + 0.1 * g - 30, 0, 255);
+            g = ofClamp(0.9 * r + 0.1 * b - 30, 0, 255);
+            b = ofClamp(0.9 * b + 0.1 * g - 30, 0, 255);
 
-            r = ofClamp(r, 0, 255);
-            g = ofClamp(g, 0, 255);
-            b = ofClamp(b, 0, 255);
-
+            //r = ofClamp(0.9 * r + 0.05 * g + 0.05 * b, 0, 255);
+            //g = ofClamp(0.75 * r + 0.1 * g + 0.15 * b, 0, 255);
+            //b = ofClamp(0.6 * r + 0.2 * g + 0.2 * b, 0, 255);
+                        
             filteredImage.setColor(x, y, ofColor(r, g, b));
         }
     }
